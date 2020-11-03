4cd1574387b5d6612f85b7aa8fdf24a8afdc2616
Graham Mendick <graham.mendick@gmail.com>
2020-11-03 22:32:51 +0000

    Destructured pathname and search from location

--- src/useHistory.js
+++ src/useHistory.js
@@ -2,8 +2,9 @@ import { useEffect } from 'react';
 
 const useHistory = (page, title, setPage, setTitle, setFilter) => {
   useEffect(() => {
-    const getPage = () => +window.location.pathname.substring(1) || 1;
-    const getTitle = () => decodeURIComponent(window.location.search.substring(7));
+    const {location: {pathname, search}} = window;
+    const getPage = () => +pathname.substring(1) || 1;
+    const getTitle = () => decodeURIComponent(search.substring(7));
     const handleHistory = () => {
       setPage(getPage());
       setTitle(getTitle());
