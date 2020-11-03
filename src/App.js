b00a7573a74962385db50eedbb86ee34c1bb5c0a
Graham Mendick <graham.mendick@gmail.com>
2020-11-03 10:56:57 +0000

    Added example pagination
    Included fetch headers otherwise json body wasn't accepted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Pagination from 'react-bootstrap/Pagination';
 import './App.scss';
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [page, setPage] = useState(1);
   useEffect(() => {
     fetch('http://nyx.vima.ekt.gr:3000/api/books', {
       method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      cache: 'no-cache',
       body: JSON.stringify({
-          page: 1,
+          page,
           itemsPerPage: 20,
           filters: []
         }
@@ -16,15 +23,25 @@ function App() {
     })
     .then(res => res.json())
     .then(({books}) => setBooks(books))
-  }, []);
+  }, [page]);
   return (
-    <ListGroup as="ul">
-      {books.map(({id, book_title}) => (
-        <ListGroup.Item as="li" key={id}>
-          {book_title}
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
+    <>
+      <ListGroup as="ul">
+        {books.map(({id, book_title}) => (
+          <ListGroup.Item as="li" key={id}>
+            {book_title}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <Pagination onClick={e => {
+        if (!e.ctrlKey && !e.shiftKey && !e.metaKey && !e.altKey && !e.button) {
+          e.preventDefault();
+          setPage(20);
+        }
+      }}>
+        <Pagination.Item href="/2">{2}</Pagination.Item>
+      </Pagination>
+    </>
   );
 }
 
