b76fc4450731a9114ed561b5cce992d599ea6e62
Graham Mendick <graham.mendick@gmail.com>
2020-11-03 22:22:06 +0000

    Shared history handling function

--- src/useHistory.js
+++ src/useHistory.js
@@ -4,21 +4,18 @@ const useHistory = (page, title, setPage, setTitle, setFilter) => {
   useEffect(() => {
     const getPage = () => +window.location.pathname.substring(1) || 1;
     const getTitle = () => window.location.search.substring(7);
-    if (!page) {
-      setPage(getPage());
-      setTitle(getTitle())
-      setFilter(getTitle())
-    }
-    if (page && (page !== getPage() || title !== getTitle()))
-      window.history.pushState(null, null, 
-        `/${page !== 1 ? page : ''}${title ? '?title=' : ''}${title}`);
-    const handlePopState = () => {
+    const handleHistory = () => {
       setPage(getPage());
       setTitle(getTitle());
       setFilter(getTitle())
     };
-    window.addEventListener('popstate', handlePopState);
-    return () => window.removeEventListener('popstate', handlePopState);
+    if (!page)
+      handleHistory();
+    if (page && (page !== getPage() || title !== getTitle()))
+      window.history.pushState(null, null, 
+        `/${page !== 1 ? page : ''}${title ? '?title=' : ''}${title}`);
+    window.addEventListener('popstate', handleHistory);
+    return () => window.removeEventListener('popstate', handleHistory);
   }, [page, title, setPage, setTitle, setFilter]);
 };
 
