89cb45ecc793abfcfd6db7c130005e5c37aa026a
Graham Mendick <graham.mendick@gmail.com>
2020-11-03 19:07:41 +0000

    Unit tested the pager links

--- src/App.test.js
+++ src/App.test.js
@@ -41,3 +41,34 @@ test('renders book list', () => {
   assert.strictEqual(titles[1].innerHTML, 'book two');
   assert.strictEqual(titles[2].innerHTML, 'book three');
 });
+
+test('renders pager links', () => {
+  const fetch = mockFetch({
+    '1' : {
+      books: Array.from({length: 20}, (_, i) => (
+        {id: i, book_title: `book ${i}`}
+      )),
+      count: 250
+    }
+  });
+  const container = document.createElement('div');
+  act(() => {
+    ReactDOM.render(
+      <FetchContext.Provider value={fetch}>
+        <App />
+      </FetchContext.Provider>,
+      container);
+  });
+  const pager = container.querySelectorAll('ul')[1];
+  const pages = pager.querySelectorAll('li a');
+  assert.strictEqual(pages.length, 9);
+  assert.strictEqual(pages[0].innerHTML, '2');
+  assert.strictEqual(pages[1].innerHTML, '3');
+  assert.strictEqual(pages[2].innerHTML, '4');
+  assert.strictEqual(pages[3].innerHTML, '5');
+  assert.strictEqual(pages[4].innerHTML, '6');
+  assert.strictEqual(pages[5].innerHTML, '7');
+  assert.strictEqual(pages[6].innerHTML, '8');
+  assert.strictEqual(pages[7].innerHTML, '9');
+  assert.strictEqual(pages[8].innerHTML, '10');
+});
